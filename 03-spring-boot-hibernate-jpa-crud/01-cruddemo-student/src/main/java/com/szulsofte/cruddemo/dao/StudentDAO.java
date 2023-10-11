eb87fa4a37b804733d5fdec08652e1fc9c8725ce
Karol Szul <szul.karol94@gmail.com>
2023-10-11 20:05:16 +0200

    adding updating and deleting features

--- 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/szulsofte/cruddemo/dao/StudentDAO.java
+++ 03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/szulsofte/cruddemo/dao/StudentDAO.java
@@ -2,12 +2,24 @@ package com.szulsofte.cruddemo.dao;
 
 import com.szulsofte.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
 
     Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String theLastName);
+
+    void update (Student theStudent);
+
+    void delete (Integer id);
+
+    int deleteAll();
 
 
 }
