8d9386e9b3a78aa2127fbf5ab6ecc26ca685a09b
Karol Szul <szul.karol94@gmail.com>
2023-10-18 22:30:57 +0200

    Starting on exception handling

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/szulsoft/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/szulsoft/demo/rest/StudentRestController.java
@@ -2,7 +2,9 @@ package com.szulsoft.demo.rest;
 
 
 import com.szulsoft.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,18 +15,35 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Karol", "Szul"));
+        theStudents.add(new Student("Imbir", "Szul"));
+        theStudents.add(new Student("Alfie", "Solomons"));
+    }
+
     // define endpoint for "/students" - return list of students
 
     @GetMapping("/students")
     public List<Student> getStudents() {
 
-        List<Student> theStudents = new ArrayList<>();
-
-        theStudents.add(new Student("Karol", "Szul"));
-        theStudents.add(new Student("Imbir", "Szul"));
-        theStudents.add(new Student("Jakub", "Szapiro"));
-
         return theStudents;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        //just index into the list
+        return theStudents.get(studentId);
     }
 
 }
